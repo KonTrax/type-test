be5b606f8a5b1fced74d23d2b40c3e7605b7bf2d
KonTrax <kontraxbea+git@gmail.com>
2018-12-21 20:59:00 +0100

    cleaning

--- src/types/core.ts
+++ src/types/core.ts
@@ -8,12 +8,12 @@ export type IsBOL <T> = Equals<T, BOL>
 export type IsTRU <T> = Equals<T, TRU>
 export type IsFAL <T> = Equals<T, FAL>
 
-export type IsValue        <T>    = TC.IsValue        <T, TRU, FAL>
-export type IsSpecial      <T>    = TC.IsSpecial      <T, TRU, FAL>
-export type IsNever        <T>    = TC.IsNever        <T, TRU, FAL>
-export type IsUnknownOrAny <T>    = TC.IsUnknownOrAny <T, TRU, FAL>
-export type IsUnknown      <T>    = TC.IsUnknown      <T, TRU, FAL>
-export type IsAny          <T>    = TC.IsAny          <T, TRU, FAL>
+export type IsValue        <T> = TC.IsValue        <T, TRU, FAL>
+export type IsSpecial      <T> = TC.IsSpecial      <T, TRU, FAL>
+export type IsNever        <T> = TC.IsNever        <T, TRU, FAL>
+export type IsUnknownOrAny <T> = TC.IsUnknownOrAny <T, TRU, FAL>
+export type IsUnknown      <T> = TC.IsUnknown      <T, TRU, FAL>
+export type IsAny          <T> = TC.IsAny          <T, TRU, FAL>
 
 export type Equals  <A, B> = TC.Equals  <A, B, TRU, FAL>
 export type Accepts <A, B> = TC.Accepts <A, B, TRU, FAL>
